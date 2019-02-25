66dbce7bc2b4f2cced0ba420ce715d78ad6c7631
danielemichilli <danielemichilli@gmail.com>
2019-02-25 14:01:02 -0500

    pypi set up

--- setup.py
+++ setup.py
@@ -8,28 +8,22 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = 'DM_phase'
+NAME = 'dmphase'
 DESCRIPTION = 'Find the best Dispersion Measure by using FFT phase angle.'
 URL = 'https://github.com/danielemichilli/DM_phase'
 EMAIL = 'danielemichilli@gmail.com'
 AUTHOR = 'Andrew Seymour and Daniele Michilli'
-REQUIRES_PYTHON = '=2.7'
-VERSION = 1.0
+REQUIRES_PYTHON = '==2.7.*'
+VERSION = False
 
 # What packages are required for this module to be executed?
 REQUIRED = [
-    # 'requests', 'maya', 'records',
+    'numpy>=1.13.3',
+    'matplotlib>=2.1',
+    'scipy>=0.19.1'
 ]
 
-# What packages are optional?
-EXTRAS = {
-    # 'fancy feature': ['django'],
-}
-
-# The rest you shouldn't have to touch too much :)
 # ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -44,9 +38,8 @@ except FileNotFoundError:
 # Load the package's __version__.py module as a dictionary.
 about = {}
 if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
+    with open(os.path.join(here, '__version__.py')) as f:
+        about['__version__'] = f.read()
 else:
     about['__version__'] = VERSION
 
@@ -70,18 +63,18 @@ class UploadCommand(Command):
 
     def run(self):
         try:
-            self.status('Removing previous builds…')
+            self.status('Removing previous builds')
             rmtree(os.path.join(here, 'dist'))
         except OSError:
             pass
 
-        self.status('Building Source and Wheel (universal) distribution…')
+        self.status('Building Source and Wheel (universal) distribution')
         os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
 
-        self.status('Uploading the package to PyPI via Twine…')
+        self.status('Uploading the package to PyPI via Twine')
         os.system('twine upload dist/*')
 
-        self.status('Pushing git tags…')
+        self.status('Pushing git tags')
         os.system('git tag v{0}'.format(about['__version__']))
         os.system('git push --tags')
         
@@ -99,20 +92,16 @@ setup(
     author_email=EMAIL,
     python_requires=REQUIRES_PYTHON,
     url=URL,
-    packages=find_packages(exclude=('tests',)),
-    # If your package is a single module, use this instead of 'packages':
-    # py_modules=['mypackage'],
-
-    # entry_points={
-    #     'console_scripts': ['mycli=mymodule:cli'],
-    # },
+    py_modules=['DM_phase'],
+    entry_points={
+         'console_scripts': ['DM_phase=DM_phase:from_PSRCHIVE'],
+     },
     install_requires=REQUIRED,
-    extras_require=EXTRAS,
     include_package_data=True,
     license='GNU',
     classifiers=[
-        # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Operating System :: Unix",
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7'
